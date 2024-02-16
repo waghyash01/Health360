b090855747705501f4146bf49e7769463ba19078
waghyash01 <yash.wagh_comp22@pccoer.in>
2024-02-16 13:19:10 +0530

    commit by dharmesh user,doctor and depatientdata UPDATED

--- lib/DecryptPatientData.dart
+++ lib/DecryptPatientData.dart
@@ -24,6 +24,8 @@ class _AESDecryptionPageState extends State<AESDecryptionPage> {
   CollectionReference _encryptedDataCollection =
   FirebaseFirestore.instance.collection('Patients');
   List<String> decryptedDataList = [];
+  TextEditingController emailController = TextEditingController();
+  TextEditingController patientIdController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -37,18 +39,21 @@ class _AESDecryptionPageState extends State<AESDecryptionPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            InkWell(
-              onTap: () {
+            TextFormField(
+              controller: emailController,
+              decoration: InputDecoration(labelText: 'For Additional Security ReEnter Email'),
+            ),
+            SizedBox(height: 16.0),
+            TextFormField(
+              controller: patientIdController,
+              decoration: InputDecoration(labelText: 'For Additional Security ReEnter Password'),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
                 _decryptAndShowData();
               },
-              child: Container(
-                padding: EdgeInsets.all(16.0),
-                color: Colors.lightBlueAccent,
-                child: Text(
-                  'Tap to view Records',
-                  style: TextStyle(color: Colors.black),
-                ),
-              ),
+              child: Text('Tap to view Records'),
             ),
             SizedBox(height: 16.0),
             Expanded(
@@ -69,49 +74,43 @@ class _AESDecryptionPageState extends State<AESDecryptionPage> {
   }
 
   Future<void> _decryptAndShowData() async {
-    // Retrieve the currently authenticated user
-    User? user = FirebaseAuth.instance.currentUser;
+    String enteredEmail = emailController.text.trim();
+    String enteredPatientId = patientIdController.text.trim();
 
-    if (user != null) {
-      // Retrieve the email of the authenticated user
-      String loggedInUserEmail = user.email ?? '';
+    QuerySnapshot<Map<String, dynamic>> querySnapshot =
+    await _encryptedDataCollection
+        .where('Email', isEqualTo: enteredEmail)
+        .where('Patient ID', isEqualTo: enteredPatientId)
+        .limit(1)
+        .get() as QuerySnapshot<Map<String, dynamic>>; // Specify the type
 
-      // Retrieve encrypted data from Firestore using the logged-in user's email as the document ID
-      DocumentSnapshot<Map<String, dynamic>> snapshot =
-      await _encryptedDataCollection.doc(loggedInUserEmail).get()
-      as DocumentSnapshot<Map<String, dynamic>>; // Explicitly specify the type
-
-      if (snapshot.exists) {
-        // Decrypt the encrypted data
-        Map<String, dynamic>? encryptedData = snapshot.data(); // Change type to nullable
-        decryptedDataList.clear();
-        if (encryptedData != null) {
-          decryptedDataList.addAll([
-            AESAlgorithm.decryptData(encryptedData['Full Name'] ?? ''),
-            _decryptAge(encryptedData['Age']),
-            AESAlgorithm.decryptData(encryptedData['Gender'] ?? ''),
-            AESAlgorithm.decryptData(encryptedData['Contact Number'] ?? ''),
-            encryptedData['Patient Id'] ?? '',
-            loggedInUserEmail, // Display the logged-in user's email
-            AESAlgorithm.decryptData(encryptedData['Address'] ?? ''),
-            AESAlgorithm.decryptData(encryptedData['Blood Group'] ?? ''),
-            AESAlgorithm.decryptData(encryptedData['Medical History'] ?? ''),
-            // Add decryption for other fields here
-          ]);
-        } else {
-          // Handle case when encryptedData is null
-          // For example, show a snackbar or display a message to the user
-        }
-
-        setState(() {});
+    if (querySnapshot.docs.isNotEmpty) {
+      Map<String, dynamic>? encryptedData = querySnapshot.docs.first.data();
+      decryptedDataList.clear();
+      if (encryptedData != null) {
+        decryptedDataList.addAll([
+          AESAlgorithm.decryptData(encryptedData['Full Name'] ?? ''),
+          _decryptAge(encryptedData['Age']),
+          AESAlgorithm.decryptData(encryptedData['Gender'] ?? ''),
+          AESAlgorithm.decryptData(encryptedData['Contact Number'] ?? ''),
+          encryptedData['Patient ID'] ?? '',
+          enteredEmail,
+          AESAlgorithm.decryptData(encryptedData['Address'] ?? ''),
+          AESAlgorithm.decryptData(encryptedData['Blood Group'] ?? ''),
+          AESAlgorithm.decryptData(encryptedData['Medical History'] ?? ''),
+        ]);
       } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text(
-                'No encrypted data found for the logged-in user!'),
-          ),
-        );
+        // Handle case when encryptedData is null
+        // For example, show a snackbar or display a message to the user
       }
+
+      setState(() {});
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('No data found for the provided email and patient ID.'),
+        ),
+      );
     }
   }
 
@@ -125,125 +124,3 @@ class _AESDecryptionPageState extends State<AESDecryptionPage> {
     }
   }
 }
-
-
-
-// import 'package:flutter/material.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'aesAlgorithm.dart';
-//
-// List<String> patientdetails = [
-//   'Full Name',
-//   'Age',
-//   'Gender',
-//   'Contact Number',
-//   'Patient Id',
-//   'Email',
-//   'Address',
-//   'Blood Group',
-//   'Medical History',
-// ];
-//
-// class AESDecryptionPage extends StatefulWidget {
-//   @override
-//   _AESDecryptionPageState createState() => _AESDecryptionPageState();
-// }
-//
-// class _AESDecryptionPageState extends State<AESDecryptionPage> {
-//   CollectionReference _encryptedDataCollection =
-//   FirebaseFirestore.instance.collection('encrypted_data');
-//   List<String> decryptedDataList = [];
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('AES Encryption Demo'),
-//       ),
-//       body: Padding(
-//         padding: EdgeInsets.all(16.0),
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.stretch,
-//           children: [
-//             InkWell(
-//               onTap: () {
-//                 _decryptAndShowData();
-//               },
-//               child: Container(
-//                 padding: EdgeInsets.all(16.0),
-//                 color: Colors.blue,
-//                 child: Text(
-//                   'Tap here to view your encrypted data',
-//                   style: TextStyle(color: Colors.white),
-//                 ),
-//               ),
-//             ),
-//             SizedBox(height: 16.0),
-//             Expanded(
-//               child: ListView.builder(
-//                 itemCount: decryptedDataList.length,
-//                 itemBuilder: (context, index) {
-//                   return ListTile(
-//                     title: Text(patientdetails[index]),
-//                     subtitle: Text(decryptedDataList[index]),
-//                   );
-//                 },
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-//
-//   Future<void> _decryptAndShowData() async {
-//     // Retrieve the logged-in user's email
-//     String loggedInUserEmail = 'kom@gmail.com';
-//
-//     // Retrieve encrypted data from Firestore using the logged-in user's email as the document ID
-//     DocumentSnapshot<Map<String, dynamic>> snapshot =
-//     await _encryptedDataCollection.doc(loggedInUserEmail).get()
-//     as DocumentSnapshot<Map<String, dynamic>>; // Explicitly specify the type
-//
-//     if (snapshot.exists) {
-//       // Decrypt the encrypted data
-//       Map<String, dynamic>? encryptedData = snapshot.data(); // Change type to nullable
-//       decryptedDataList.clear();
-//       if (encryptedData != null) {
-//         decryptedDataList.addAll([
-//           AESAlgorithm.decryptData(encryptedData['Full Name'] ?? ''),
-//           _decryptAge(encryptedData['Age']),
-//           AESAlgorithm.decryptData(encryptedData['Gender'] ?? ''),
-//           AESAlgorithm.decryptData(encryptedData['Contact Number'] ?? ''),
-//           encryptedData['Patient Id'] ?? '',
-//           loggedInUserEmail, // Display the logged-in user's email
-//           AESAlgorithm.decryptData(encryptedData['Address'] ?? ''),
-//           AESAlgorithm.decryptData(encryptedData['Blood Group'] ?? ''),
-//           AESAlgorithm.decryptData(encryptedData['Medical History'] ?? ''),
-//           // Add decryption for other fields here
-//         ]);
-//       } else {
-//         // Handle case when encryptedData is null
-//         // For example, show a snackbar or display a message to the user
-//       }
-//
-//       setState(() {});
-//     } else {
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         SnackBar(
-//           content: Text('No encrypted data found for the logged-in user!'),
-//         ),
-//       );
-//     }
-//   }
-//
-//   String _decryptAge(dynamic ageData) {
-//     if (ageData is int) {
-//       // If age is already an integer, return it as a string
-//       return ageData.toString();
-//     } else {
-//       // If age is not an integer, decrypt it
-//       return AESAlgorithm.decryptData(ageData.toString());
-//     }
-//   }
-// }
--- lib/loginnew.dart
+++ lib/loginnew.dart
@@ -10,7 +10,6 @@ class UserTypeSelectionPage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.greenAccent,
         title: Text('Select User'),
-
       ),
       body: Center(
         child: Column(
@@ -23,10 +22,15 @@ class UserTypeSelectionPage extends StatelessWidget {
                   MaterialPageRoute(builder: (context) => PatientLoginPage()),
                 );
               },
-              child: Text('Patient'),
-
+              child: Column(
+                children: [
+                  Container(child: Image.asset('assets/images/user.jpg',fit: BoxFit.cover, height:100 , width:100)),// Add Patient image
+                  SizedBox(height: 8.0),
+                  Text('Patient'),
+                ],
+              ),
             ),
-            SizedBox(height: 16.0),
+            SizedBox(height: 70.0),
             ElevatedButton(
               onPressed: () {
                 Navigator.push(
@@ -34,7 +38,13 @@ class UserTypeSelectionPage extends StatelessWidget {
                   MaterialPageRoute(builder: (context) => DoctorLoginPage()),
                 );
               },
-              child: Text('Doctor'),
+              child: Column(
+                children: [
+                  Image.asset('assets/images/doctor.png', height: 100, width: 100), // Add Doctor image
+                  SizedBox(height: 8.0),
+                  Text('Doctor'),
+                ],
+              ),
             ),
           ],
         ),
@@ -75,7 +85,6 @@ class PatientLoginPage extends StatelessWidget {
                 login(context, 'Patients', loginEmailController.text, loginPatientIdController.text);
               },
               child: Text('Login'),
-
             ),
           ],
         ),
@@ -185,3 +194,194 @@ void main() {
     home: UserTypeSelectionPage(),
   ));
 }
+
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:sampleandro/Doctor.dart';
+// import 'package:sampleandro/patient.dart';
+//
+// class UserTypeSelectionPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.greenAccent,
+//         title: Text('Select User'),
+//
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             ElevatedButton(
+//               onPressed: () {
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(builder: (context) => PatientLoginPage()),
+//                 );
+//               },
+//               child: Text('Patient'),
+//
+//             ),
+//             SizedBox(height: 16.0),
+//             ElevatedButton(
+//               onPressed: () {
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(builder: (context) => DoctorLoginPage()),
+//                 );
+//               },
+//               child: Text('Doctor'),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
+// class PatientLoginPage extends StatelessWidget {
+//   final TextEditingController loginEmailController = TextEditingController();
+//   final TextEditingController loginPatientIdController = TextEditingController();
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.greenAccent,
+//         title: Text('Patient Login'),
+//       ),
+//       body: Padding(
+//         padding: EdgeInsets.all(16.0),
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             TextField(
+//               controller: loginEmailController,
+//               decoration: InputDecoration(labelText: 'Email'),
+//             ),
+//             SizedBox(height: 8.0),
+//             TextField(
+//               controller: loginPatientIdController,
+//               decoration: InputDecoration(labelText: 'Patient ID'),
+//             ),
+//             SizedBox(height: 16.0),
+//             ElevatedButton(
+//               style: ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.greenAccent)),
+//               onPressed: () {
+//                 login(context, 'Patients', loginEmailController.text, loginPatientIdController.text);
+//               },
+//               child: Text('Login'),
+//
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+//
+//   void login(BuildContext context, String collection, String email, String patientId) async {
+//     try {
+//       var result = await FirebaseFirestore.instance.collection(collection).where('Email', isEqualTo: email).where('Patient ID', isEqualTo: patientId).get();
+//
+//       if (result.docs.isNotEmpty) {
+//         Navigator.pushReplacement(
+//           context,
+//           MaterialPageRoute(builder: (context) => Patientpage()),
+//         );
+//       } else {
+//         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+//           content: Text('Invalid login credentials. Patient not found.'),
+//         ));
+//       }
+//     } catch (e) {
+//       print('Error during login: $e');
+//     }
+//   }
+// }
+//
+// class DoctorLoginPage extends StatelessWidget {
+//   final TextEditingController loginNameController = TextEditingController();
+//   final TextEditingController loginUidController = TextEditingController();
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.greenAccent,
+//         title: Text('Doctor Login'),
+//       ),
+//       body: Padding(
+//         padding: EdgeInsets.all(16.0),
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             TextField(
+//               controller: loginNameController,
+//               decoration: InputDecoration(labelText: 'Doctor Name'),
+//             ),
+//             SizedBox(height: 8.0),
+//             TextField(
+//               controller: loginUidController,
+//               decoration: InputDecoration(labelText: 'Doctor UID'),
+//             ),
+//             SizedBox(height: 16.0),
+//             ElevatedButton(
+//               style: ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.greenAccent)),
+//               onPressed: () {
+//                 login(context, 'Doctors', loginNameController.text, loginUidController.text);
+//               },
+//               child: Text('Login'),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+//
+//   void login(BuildContext context, String collection, String name, String uid) async {
+//     try {
+//       var result = await FirebaseFirestore.instance.collection(collection).where('Name', isEqualTo: name).where('UID', isEqualTo: uid).get();
+//
+//       if (result.docs.isNotEmpty) {
+//         Navigator.pushReplacement(
+//           context,
+//           MaterialPageRoute(builder: (context) => Doctorpage()),
+//         );
+//       } else {
+//         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+//           content: Text('Invalid login credentials. Doctor not found.'),
+//         ));
+//       }
+//     } catch (e) {
+//       print('Error during login: $e');
+//     }
+//   }
+// }
+//
+// class HomeScreen extends StatelessWidget {
+//   final String userType;
+//
+//   HomeScreen({required this.userType});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Home Screen - $userType'),
+//       ),
+//       body: Center(
+//         child: Text('Welcome to the Home Screen, $userType!'),
+//       ),
+//     );
+//   }
+// }
+//
+// void main() {
+//   runApp(MaterialApp(
+//     home: UserTypeSelectionPage(),
+//   ));
+// }
